91bd0f7c526eaefeb296fd397a0ff5ca0bd08980
trakker01 <91250136+trakker01@users.noreply.github.com>
2023-04-01 21:22:53 +0300

    Crearea functiei de decriptare pentru cezar cypher
    Creare functiei de decripate pentru cezar cypher si adaugarea a unor label-uri , un textbox, un combo box care vor fi folosite pentru interfata grafica a proiectului

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textCriptat = new System.Windows.Forms.Label();
+            this.textDecryptat = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(254, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Introdu textul care doresti sa decriptezi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Alege un cipher pentru criptare si decriptare";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Cezar",
+            "Vigenere"});
+            this.comboBox1.Location = new System.Drawing.Point(254, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Text criptat";
+            // 
+            // textCriptat
+            // 
+            this.textCriptat.AutoSize = true;
+            this.textCriptat.Location = new System.Drawing.Point(91, 70);
+            this.textCriptat.Name = "textCriptat";
+            this.textCriptat.Size = new System.Drawing.Size(10, 13);
+            this.textCriptat.TabIndex = 5;
+            this.textCriptat.Text = "-";
+            // 
+            // textDecryptat
+            // 
+            this.textDecryptat.AutoSize = true;
+            this.textDecryptat.Location = new System.Drawing.Point(91, 95);
+            this.textDecryptat.Name = "textDecryptat";
+            this.textDecryptat.Size = new System.Drawing.Size(10, 13);
+            this.textDecryptat.TabIndex = 7;
+            this.textDecryptat.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Text decryptat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textDecryptat);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textCriptat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label textCriptat;
+        private System.Windows.Forms.Label textDecryptat;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -18,5 +18,36 @@ namespace proiect_securitate
             //Prelucrat de maxim
             //Comentat de Iulia_E
         }
+
+        public string Cezar_decrypt(string crypetd_text,int shift_key)
+        {
+            string decrypted_text = string.Empty;
+            
+            foreach(char c in crypetd_text)
+            {
+                if (char.IsLetter(c))
+                {
+                    bool isUpper = char.IsUpper(c);
+                    char letter;
+                    letter = char.ToLower(c);
+                    char shifted_letter = (char)(((letter - shift_key - 'a' + 26) % 26) + 'a');
+                    if (isUpper)
+                    {
+                        shifted_letter = char.ToUpper(shifted_letter);
+                        decrypted_text += shifted_letter;
+                    }
+                    else
+                    { 
+                        decrypted_text += shifted_letter;
+                    }
+                }
+                else
+                {
+                    decrypted_text+= c;
+                }
+            }
+
+            return decrypted_text;
+        } 
     }
 }
