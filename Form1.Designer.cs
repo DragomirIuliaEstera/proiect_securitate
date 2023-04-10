80c5f7ef7498bf03956528a050f871e3d5c83a91
Dragomir- Ficard Iulia- Estera <31253349+DragomirIuliaEstera@users.noreply.github.com>
2023-04-10 14:43:23 +0300

    changed to 2 options app

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,19 +33,26 @@
             this.label2 = new System.Windows.Forms.Label();
             this.combo_box_cipher_chosen = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textCriptat = new System.Windows.Forms.Label();
-            this.textDecryptat = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.Text_cheie = new System.Windows.Forms.Label();
             this.text_box_key = new System.Windows.Forms.TextBox();
             this.button_cipher = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_server = new System.Windows.Forms.Button();
+            this.btn_client = new System.Windows.Forms.Button();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.textCriptat = new System.Windows.Forms.TextBox();
+            this.textDecriptat = new System.Windows.Forms.TextBox();
+            this.label1b = new System.Windows.Forms.Label();
+            this.label2b = new System.Windows.Forms.Label();
+            this.text_cheie_decriptare = new System.Windows.Forms.Label();
+            this.text_to_decrypt = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // text_to_encrypt
             // 
             this.text_to_encrypt.Location = new System.Drawing.Point(339, 7);
-            this.text_to_encrypt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.text_to_encrypt.Margin = new System.Windows.Forms.Padding(4);
             this.text_to_encrypt.Name = "text_to_encrypt";
             this.text_to_encrypt.Size = new System.Drawing.Size(359, 22);
             this.text_to_encrypt.TabIndex = 0;
@@ -56,9 +63,9 @@
             this.label1.Location = new System.Drawing.Point(16, 11);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(252, 17);
+            this.label1.Size = new System.Drawing.Size(264, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Introdu textul care doresti sa decriptezi";
+            this.label1.Text = "Introdu textul pe care doresti sa-l criptezi";
             // 
             // label2
             // 
@@ -66,9 +73,9 @@
             this.label2.Location = new System.Drawing.Point(16, 50);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(286, 17);
+            this.label2.Size = new System.Drawing.Size(204, 17);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Alege un cipher pentru criptare si decriptare";
+            this.label2.Text = "Alege un cipher pentru criptare";
             // 
             // combo_box_cipher_chosen
             // 
@@ -77,7 +84,7 @@
             "Cezar",
             "Vigenere"});
             this.combo_box_cipher_chosen.Location = new System.Drawing.Point(339, 47);
-            this.combo_box_cipher_chosen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.combo_box_cipher_chosen.Margin = new System.Windows.Forms.Padding(4);
             this.combo_box_cipher_chosen.Name = "combo_box_cipher_chosen";
             this.combo_box_cipher_chosen.Size = new System.Drawing.Size(160, 24);
             this.combo_box_cipher_chosen.TabIndex = 3;
@@ -92,35 +99,15 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Text criptat";
             // 
-            // textCriptat
-            // 
-            this.textCriptat.AutoSize = true;
-            this.textCriptat.Location = new System.Drawing.Point(117, 214);
-            this.textCriptat.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.textCriptat.Name = "textCriptat";
-            this.textCriptat.Size = new System.Drawing.Size(13, 17);
-            this.textCriptat.TabIndex = 5;
-            this.textCriptat.Text = "-";
-            // 
-            // textDecryptat
-            // 
-            this.textDecryptat.AutoSize = true;
-            this.textDecryptat.Location = new System.Drawing.Point(117, 245);
-            this.textDecryptat.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.textDecryptat.Name = "textDecryptat";
-            this.textDecryptat.Size = new System.Drawing.Size(13, 17);
-            this.textDecryptat.TabIndex = 7;
-            this.textDecryptat.Text = "-";
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(16, 245);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(98, 17);
+            this.label6.Size = new System.Drawing.Size(94, 17);
             this.label6.TabIndex = 6;
-            this.label6.Text = "Text decryptat";
+            this.label6.Text = "Text decriptat";
             // 
             // Text_cheie
             // 
@@ -134,8 +121,8 @@
             // 
             // text_box_key
             // 
-            this.text_box_key.Location = new System.Drawing.Point(196, 85);
-            this.text_box_key.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.text_box_key.Location = new System.Drawing.Point(207, 85);
+            this.text_box_key.Margin = new System.Windows.Forms.Padding(4);
             this.text_box_key.Name = "text_box_key";
             this.text_box_key.Size = new System.Drawing.Size(132, 22);
             this.text_box_key.TabIndex = 9;
@@ -143,7 +130,7 @@
             // button_cipher
             // 
             this.button_cipher.Location = new System.Drawing.Point(20, 128);
-            this.button_cipher.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_cipher.Margin = new System.Windows.Forms.Padding(4);
             this.button_cipher.Name = "button_cipher";
             this.button_cipher.Size = new System.Drawing.Size(100, 58);
             this.button_cipher.TabIndex = 10;
@@ -151,34 +138,128 @@
             this.button_cipher.UseVisualStyleBackColor = true;
             this.button_cipher.Click += new System.EventHandler(this.button_cipher_Click);
             // 
-            // button1
+            // label4
             // 
-            this.button1.Location = new System.Drawing.Point(788, 46);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "test iron";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(392, 90);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Alege o optiune";
+            // 
+            // btn_server
+            // 
+            this.btn_server.Location = new System.Drawing.Point(228, 137);
+            this.btn_server.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_server.Name = "btn_server";
+            this.btn_server.Size = new System.Drawing.Size(199, 58);
+            this.btn_server.TabIndex = 12;
+            this.btn_server.Text = "Server";
+            this.btn_server.UseVisualStyleBackColor = true;
+            this.btn_server.Click += new System.EventHandler(this.btn_server_Click);
+            // 
+            // btn_client
+            // 
+            this.btn_client.Location = new System.Drawing.Point(456, 137);
+            this.btn_client.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_client.Name = "btn_client";
+            this.btn_client.Size = new System.Drawing.Size(199, 58);
+            this.btn_client.TabIndex = 13;
+            this.btn_client.Text = "Client";
+            this.btn_client.UseVisualStyleBackColor = true;
+            this.btn_client.Click += new System.EventHandler(this.btn_client_Click);
+            // 
+            // btn_back
+            // 
+            this.btn_back.Location = new System.Drawing.Point(139, 128);
+            this.btn_back.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(100, 58);
+            this.btn_back.TabIndex = 14;
+            this.btn_back.Text = "Inapoi";
+            this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // textCriptat
+            // 
+            this.textCriptat.Location = new System.Drawing.Point(120, 211);
+            this.textCriptat.Margin = new System.Windows.Forms.Padding(4);
+            this.textCriptat.Name = "textCriptat";
+            this.textCriptat.Size = new System.Drawing.Size(132, 22);
+            this.textCriptat.TabIndex = 15;
+            // 
+            // textDecriptat
+            // 
+            this.textDecriptat.Location = new System.Drawing.Point(120, 242);
+            this.textDecriptat.Margin = new System.Windows.Forms.Padding(4);
+            this.textDecriptat.Name = "textDecriptat";
+            this.textDecriptat.Size = new System.Drawing.Size(132, 22);
+            this.textDecriptat.TabIndex = 16;
+            // 
+            // label1b
+            // 
+            this.label1b.AutoSize = true;
+            this.label1b.Location = new System.Drawing.Point(17, 12);
+            this.label1b.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1b.Name = "label1b";
+            this.label1b.Size = new System.Drawing.Size(280, 17);
+            this.label1b.TabIndex = 17;
+            this.label1b.Text = "Introdu textul pe care doresti sa-l decriptezi";
+            // 
+            // label2b
+            // 
+            this.label2b.AutoSize = true;
+            this.label2b.Location = new System.Drawing.Point(17, 50);
+            this.label2b.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2b.Name = "label2b";
+            this.label2b.Size = new System.Drawing.Size(220, 17);
+            this.label2b.TabIndex = 18;
+            this.label2b.Text = "Alege un cipher pentru decriptare";
+            // 
+            // text_cheie_decriptare
+            // 
+            this.text_cheie_decriptare.AutoSize = true;
+            this.text_cheie_decriptare.Location = new System.Drawing.Point(17, 85);
+            this.text_cheie_decriptare.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.text_cheie_decriptare.Name = "text_cheie_decriptare";
+            this.text_cheie_decriptare.Size = new System.Drawing.Size(182, 17);
+            this.text_cheie_decriptare.TabIndex = 19;
+            this.text_cheie_decriptare.Text = "Introdu cheia de decriptare:";
+            // 
+            // text_to_decrypt
+            // 
+            this.text_to_decrypt.Location = new System.Drawing.Point(339, 8);
+            this.text_to_decrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.text_to_decrypt.Name = "text_to_decrypt";
+            this.text_to_decrypt.Size = new System.Drawing.Size(359, 22);
+            this.text_to_decrypt.TabIndex = 20;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.text_to_decrypt);
+            this.Controls.Add(this.text_cheie_decriptare);
+            this.Controls.Add(this.label2b);
+            this.Controls.Add(this.label1b);
+            this.Controls.Add(this.textDecriptat);
+            this.Controls.Add(this.textCriptat);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.btn_client);
+            this.Controls.Add(this.btn_server);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button_cipher);
             this.Controls.Add(this.text_box_key);
             this.Controls.Add(this.Text_cheie);
-            this.Controls.Add(this.textDecryptat);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textCriptat);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.combo_box_cipher_chosen);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.text_to_encrypt);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -193,13 +274,20 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox combo_box_cipher_chosen;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label textCriptat;
-        private System.Windows.Forms.Label textDecryptat;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label Text_cheie;
         private System.Windows.Forms.TextBox text_box_key;
         private System.Windows.Forms.Button button_cipher;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_server;
+        private System.Windows.Forms.Button btn_client;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.TextBox textCriptat;
+        private System.Windows.Forms.TextBox textDecriptat;
+        private System.Windows.Forms.Label label1b;
+        private System.Windows.Forms.Label label2b;
+        private System.Windows.Forms.Label text_cheie_decriptare;
+        private System.Windows.Forms.TextBox text_to_decrypt;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,14 +16,14 @@ namespace proiect_securitate
 {
     public partial class Form1 : Form
     {
+
+        bool serverFlag;
+        bool clientFlag;
         public Form1()
         {
             InitializeComponent();
-           
-            label3.Visible = false;
-            label6.Visible = false;
-            textDecryptat.Visible = false;
-            textCriptat.Visible = false;
+
+            load_form();
         }
 
         public string Cezar_encrypt(string plaintext, int shift_key)
@@ -141,92 +141,156 @@ namespace proiect_securitate
             return encrypted_text;
         }
 
-        public string Vigenere_decryption(string plain_text, string key)
-        {//
-            string encrypted_text = string.Empty;
+        public string Vigenere_decryption(string encrypted_text, string key)
+        {
+            string decrypted_text = string.Empty;
             int key_length = key.Length;
             int index = 0;
 
-            foreach (char c in plain_text)
+            foreach (char c in encrypted_text)
             {
                 if (char.IsLetter(c))
                 {
                     bool isUpper = char.IsUpper(c);
                     char lower_letter = char.ToLower(c);
                     int key_shift = ((int)key[index]) - 97;
-                    int encrypted_letter = (int)lower_letter + key_shift;
+                    int decrypted_letter = (int)lower_letter + key_shift;
 
-                    if (encrypted_letter > 122)
+                    if (decrypted_letter > 122)
                     {
-                        encrypted_letter -= 26;
+                        decrypted_letter -= 26;
                     }
 
                     if (isUpper)
                     {
-                        encrypted_letter -= 32;
+                        decrypted_letter -= 32;
                     }
 
-                    encrypted_text += ((char)encrypted_letter).ToString();
+                    decrypted_text += ((char)decrypted_letter).ToString();
 
                     index = (index + 1) % key_length;
                 }
                 else
                 {
-                    encrypted_text += c;
+                    decrypted_text += c;
                 }
             }
 
-            return encrypted_text;
+            return decrypted_text;
         }
 
         private void button_cipher_Click(object sender, EventArgs e)
         {
-            label3.Visible = true;
-            label6.Visible = true;
-            textDecryptat.Visible = true;
-            textCriptat.Visible = true;
-
-            if(combo_box_cipher_chosen.Text.Equals("Cezar") && text_box_key.Text.ToString() != "" && text_to_encrypt.Text.ToString() != "")
+            if(serverFlag == true)
             {
-                try
+                label6.Visible = true;
+                textDecriptat.Visible = true;
+                if (combo_box_cipher_chosen.Text.Equals("Cezar") && text_box_key.Text.ToString() != "" && text_to_decrypt.Text.ToString() != "")
                 {
-                    int shift_key = Int32.Parse(text_box_key.Text.ToString());
-                    string input = text_to_encrypt.Text.ToString();
-                    textCriptat.Text = Cezar_encrypt(input, shift_key);
-                    textDecryptat.Text = Cezar_decrypt(textCriptat.Text, shift_key);
+                    try
+                    {
+                        int shift_key = Int32.Parse(text_box_key.Text.ToString());
+                        string input = text_to_decrypt.Text.ToString();
+                        textDecriptat.Text = Cezar_decrypt(input, shift_key);
+                    }
+                    catch (Exception ex) { textDecriptat.Text = "Cheia trebuie sa fie un numar"; }
                 }
-                catch(Exception ex) { textCriptat.Text = "Cheia trebuie sa fie un numar"; }
-            }
-            else 
-                if(combo_box_cipher_chosen.Text.Equals("Vigenere") && text_box_key.Text.ToString() != "" && text_to_encrypt.Text.ToString() != "")
+                else
+                if (combo_box_cipher_chosen.Text.Equals("Vigenere") && text_box_key.Text.ToString() != "" && text_to_decrypt.Text.ToString() != "")
                 {
-                    string input= text_to_encrypt.Text.ToString();
+                    string input = text_to_decrypt.Text.ToString();
                     string key = text_box_key.Text.ToString().ToLower();
-                    textCriptat.Text = Vigenere_encryption(input,key);
-                    //textDecryptat.Text = Cezar_decrypt(textCriptat.Text,shift_key);
+                    textDecriptat.Text = Vigenere_encryption(input, key);
+                }
+            }
+            else if(clientFlag == true)
+            {
+                label3.Visible = true;
+                textCriptat.Visible = true;
+                if (combo_box_cipher_chosen.Text.Equals("Cezar") && text_box_key.Text.ToString() != "" && text_to_encrypt.Text.ToString() != "")
+                {
+                    try
+                    {
+                        int shift_key = Int32.Parse(text_box_key.Text.ToString());
+                        string input = text_to_encrypt.Text.ToString();
+                        textCriptat.Text = Cezar_encrypt(input, shift_key);
+                    }
+                    catch (Exception ex) { textCriptat.Text = "Cheia trebuie sa fie un numar"; }
+                }
+                else
+                if (combo_box_cipher_chosen.Text.Equals("Vigenere") && text_box_key.Text.ToString() != "" && text_to_encrypt.Text.ToString() != "")
+                {
+                    string input = text_to_encrypt.Text.ToString();
+                    string key = text_box_key.Text.ToString().ToLower();
+                    textCriptat.Text = Vigenere_encryption(input, key);
+                }
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn_server_Click(object sender, EventArgs e)
         {
-            run_cmd("", "");
+            serverFlag = true;
+            label4.Visible = false;
+            btn_server.Visible = false;
+            btn_client.Visible = false;
+
+            label1b.Visible = true;
+            label2b.Visible = true;
+            text_to_decrypt.Visible = true;
+            text_cheie_decriptare.Visible = true;
+            
+            combo_box_cipher_chosen.Visible = true;
+            text_box_key.Visible = true;
+            button_cipher.Visible = true;
+            btn_back.Visible = true;
         }
 
-        public void run_cmd(string cmd, string args)
+        private void btn_client_Click(object sender, EventArgs e)
         {
-            try
-            {
-                ProcessStartInfo start = new ProcessStartInfo();
-                start.FileName = "server1.exe";
-                //start.Arguments = string.Format("\"{0}\" \"{1}\"", cmd, args);
-                start.UseShellExecute = true;// Do not use OS shell
-                start.CreateNoWindow = false; // We don't need new window
-                using (Process process = Process.Start(start))
-                {
+            clientFlag = true;
+            label4.Visible = false;
+            btn_server.Visible = false;
+            btn_client.Visible = false;
 
-                }
-            }catch(Exception e) { }
-            //Console.WriteLine("server started");
+            label1.Visible = true;
+            label2.Visible = true;
+            Text_cheie.Visible = true;
+            text_to_encrypt.Visible = true;
+            combo_box_cipher_chosen.Visible = true;
+            text_box_key.Visible = true;
+            button_cipher.Visible = true;
+            btn_back.Visible = true;
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            load_form();
+        }
+        private void load_form()
+        {
+            label4.Visible = true;
+            btn_server.Visible = true;
+            btn_client.Visible = true;
+
+            label3.Visible = false;
+            label6.Visible = false;
+            textDecriptat.Visible = false;
+            textCriptat.Visible = false;
+
+            label1.Visible = false;
+            label2.Visible = false;
+            Text_cheie.Visible = false;
+            text_to_encrypt.Visible = false;
+            combo_box_cipher_chosen.Visible = false;
+            text_box_key.Visible = false;
+            button_cipher.Visible = false;
+            btn_back.Visible = false;
+            label1b.Visible = false;
+            label2b.Visible = false;
+            text_to_decrypt.Visible = false;
+            text_cheie_decriptare.Visible = false;
+            serverFlag = false;
+            clientFlag = false;
         }
     }
 }
